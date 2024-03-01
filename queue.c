3c6484e333490004e5314fe232808635e81c7990
ihost1002 <ihost1002@gmail.com>
2024-03-01 21:14:14 +0800

    Implement q_size()
    What i do: it will return number of elements in queue, return 0 if
    queue is NULL or empty.

--- queue.c
+++ queue.c
@@ -47,7 +47,15 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head || list_empty(head))
+        return 0;
+    int size = 0;
+    const struct list_head *count = head;
+    while (count->next != head) {
+        count = count->next;
+        size++;
+    }
+    return size;
 }
 
 /* Delete the middle node in queue */
