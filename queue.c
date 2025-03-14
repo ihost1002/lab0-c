021c66dcb9aee17022b8d245bd4caae9c1bd9963
ihost1002 <ihost1002@gmail.com>
2025-03-14 19:02:05 +0800

    Implement q_size
    Return number of elements in queue, zero if queue is NULL or empty.
    
    Change-Id: Idb6549bcfef16709fbd25a179f801a3b8b266b4d

--- queue.c
+++ queue.c
@@ -40,7 +40,14 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    /* Return zero if queue is NULL or empty */
+    if (!head || list_empty(head))
+        return 0;
+    int size = 0;
+    const struct list_head *node = NULL;
+    list_for_each(node, head)
+        size++;
+    return size;
 }
 
 /* Delete the middle node in queue */
